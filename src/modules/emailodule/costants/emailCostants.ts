0eafa778eb582c873949511f338d5ca6fdcc783e
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-10-02 13:43:16 +0300

    and with posts.controller.ts

---
+++ src/modules/emailodule/costants/emailCostants.ts
@@ -0,0 +1,17 @@
+import { EmailEnum } from '../enums/emailEnam';
+
+export const emailConstant = {
+  [EmailEnum.WELCOME]: {
+    subject: 'Welcome',
+    template: 'welcome',
+  },
+
+  [EmailEnum.OLD_VISIT]: {
+    subject: 'Forgot password',
+    template: 'old-visit',
+  },
+  [EmailEnum.GOODBYE]: {
+    subject: 'Bye',
+    template: 'goodbye',
+  },
+};
